981b250427609aa4ade93418abc74e4c316cb62c
Khoushiek <khoushiekr@gmail.com>
2020-10-21 13:44:22 +0530

    Setup created at and expense amount display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+
+const numberFormat = (value) =>
+  new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR'
+  }).format(value);
 
 export default (props) => (
   <div>
     <Link to={`/edit/${props.id}`}>
       <h3>{props.description}</h3>
     </Link>
-    <p>{props.amount} - {props.createdAt}</p>
+    <p>
+      {numberFormat(props.amount / 100 )}
+      &nbsp; - &nbsp;  
+      {moment(props.createdAt).format("MMMM Do YYYY")}</p>
   </div>
 );
